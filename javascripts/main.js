2d3b44e237f7fb4daf89583c9a8cda8843373c88
KudoCC <cangmuma@gmail.com>
2020-05-11 20:51:59 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
 console.log('This would be the main JS file.');
+
+function getGPSLocation() {
+  navigator.geolocation.getCurrentPosition(function (position) {
+       console.log(position.coords.latitude)
+       console.log(position.coords.longitude)
+    },
+    function (error) {
+        console.log("The Locator was denied. :(")
+    })
+}
+
+window.onload = function() {
+  getGPSLocation();
+};
